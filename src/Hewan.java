fc75afa2b692cd455c84e260b2da3002bd6524d6
rafipranata048 <rafipranata747@gmail.com>
2025-03-16 15:04:27 +0700

    modul 2 codelab1

---
+++ src/Hewan.java
@@ -0,0 +1,23 @@
+// Kelas Hewan
+class Hewan {
+    // Atribut
+    String nama;
+    String jenis;
+    String suara;
+
+    // Konstruktor
+    public Hewan(String nama, String jenis, String suara) {
+        this.nama = nama;
+        this.jenis = jenis;
+        this.suara = suara;
+    }
+
+    // Metode untuk menampilkan informasi hewan
+    public void tampilkanInfo() {
+        System.out.println("Nama: " + nama);
+        System.out.println("Jenis: " + jenis);
+        System.out.println("Suara: " + suara);
+        System.out.println(); // Spasi tambahan untuk kejelasan output
+    }
+}
+
